d00e7c8c7e9a97d28df9201b1d5158f6580e793d
贺敏 <hemin_it@163.com>
2016-11-04 16:54:08 +0800

    update

--- src/main/java/com/minbo/redis/cluster/ClusterTest.java
+++ src/main/java/com/minbo/redis/cluster/ClusterTest.java
@@ -6,35 +6,39 @@ import redis.clients.jedis.HostAndPort;
 import redis.clients.jedis.JedisCluster;
 import redis.clients.jedis.JedisPoolConfig;
 
+/**
+ * 集群
+ */
 public class ClusterTest {
-	public static void main(String[] args) {  
-        //初始化集合，用于装下面的多个主机和端口  
-        HashSet<HostAndPort> nodes = new HashSet<HostAndPort>();
-        String hostIp = "192.168.234.128";
-          
-        //创建多个主机和端口实例  
-        HostAndPort hostAndPort = new HostAndPort(hostIp, 7000);  
-        HostAndPort hostAndPort1 = new HostAndPort(hostIp, 7001);  
-        HostAndPort hostAndPort2 = new HostAndPort(hostIp, 7002);  
-        HostAndPort hostAndPort3 = new HostAndPort(hostIp, 7003);  
-        HostAndPort hostAndPort4 = new HostAndPort(hostIp, 7004);  
-        HostAndPort hostAndPort5 = new HostAndPort(hostIp, 7005);  
-          
-        //添加多个主机和端口到集合中  
-        nodes.add(hostAndPort);  
-        nodes.add(hostAndPort1);  
-        nodes.add(hostAndPort2);  
-        nodes.add(hostAndPort3);  
-        nodes.add(hostAndPort4);  
-        nodes.add(hostAndPort5);  
-          
-        //创建config  
-        JedisPoolConfig poolConfig = new JedisPoolConfig();  
-        //通过config创建集群实例  
-        JedisCluster jedisCluster = new JedisCluster(nodes,poolConfig);  
-        //获取集群中的key为name键的值  
-        //jedisCluster.set("age", "20");
-        System.out.println("name=" + jedisCluster.get("name"));
-        System.out.println("address=" + jedisCluster.get("address"));
-    }  
+	public static void main(String[] args) {
+		// 初始化集合，用于装下面的多个主机和端口
+		HashSet<HostAndPort> nodes = new HashSet<HostAndPort>();
+		String hostIp = "192.168.0.152";
+
+		// 创建多个主机和端口实例
+		HostAndPort hostAndPort = new HostAndPort(hostIp, 7000);
+		HostAndPort hostAndPort1 = new HostAndPort(hostIp, 7001);
+		HostAndPort hostAndPort2 = new HostAndPort(hostIp, 7002);
+		HostAndPort hostAndPort3 = new HostAndPort(hostIp, 7003);
+		HostAndPort hostAndPort4 = new HostAndPort(hostIp, 7004);
+		HostAndPort hostAndPort5 = new HostAndPort(hostIp, 7005);
+
+		// 添加多个主机和端口到集合中
+		nodes.add(hostAndPort);
+		nodes.add(hostAndPort1);
+		nodes.add(hostAndPort2);
+		nodes.add(hostAndPort3);
+		nodes.add(hostAndPort4);
+		nodes.add(hostAndPort5);
+
+		// 创建config
+		JedisPoolConfig poolConfig = new JedisPoolConfig();
+		// 通过config创建集群实例
+		JedisCluster jedisCluster = new JedisCluster(nodes, poolConfig);
+		// 获取集群中的key为name键的值
+		//jedisCluster.set("name", "hemin");
+		//System.out.println("name=" + jedisCluster.get("name"));
+		System.out.println("name=" + jedisCluster.get("name"));
+		System.out.println("address=" + jedisCluster.get("address"));
+	}
 }
